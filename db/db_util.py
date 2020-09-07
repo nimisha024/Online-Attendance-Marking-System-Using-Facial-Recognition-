bc945251457547c71d8f68072db7d2e79da53965
Nimisha Swain <niawsnimisha06@gmail.com>
2020-09-08 00:51:40 +0530

    Conflicts: 	app.py 	create_table.py 	student.py 	user.py

--- db/db_util.py
+++ db/db_util.py
@@ -5,3 +5,6 @@ import sqlite3
 def get_connection():
     db_file = os.path.join(os.getcwd(), 'db', 'data.db')
     return sqlite3.connect(db_file, isolation_level=None)
+
+ # todo change name to id
+ # add user table
