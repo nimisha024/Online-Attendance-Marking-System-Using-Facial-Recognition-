6c98e71ccc0a0f9d94bca27fbd7052bad5e952f8
Sumeet Swain <sumit231996@gmail.com>
2020-11-03 23:58:35 +0530

    feat: webcam fix

--- static/js/attendance.js
+++ static/js/attendance.js
@@ -1,3 +1,5 @@
+let img_data;
+
 function configure() {
     Webcam.set({
         width: 320,
@@ -11,22 +13,22 @@ function configure() {
 function takeSnapshot() {
     $("#upload-button").attr("disabled", false);
     Webcam.snap(function (data_uri) {
+        img_data = data_uri
         $('#result').html('<img id="image-prev" src="' + data_uri + '"/>')
     });
-
-    Webcam.reset();
 }
 
 function uploadSnap() {
     $.post({
         url: "/api/attendance/" + course_id,
-        data: {user_id: user.id, image: $("#image-prev").src},
+        data: {user_id: user.id, image: img_data},
         headers: {
             Authorization: 'JWT ' + Cookies.get('access_token')
         },
         success: function (data) {
             alert(data.message);
             $('#webcam-container').hide();
+            Webcam.reset();
         },
         error: function (e) {
             console.log("ERROR : ", e);
